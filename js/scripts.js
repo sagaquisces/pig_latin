4d7ff1b1e31e9d8c478e16139d46691ecbc34fd9
Michael Dunlap <sagaquisces@gmail.com>
2017-06-07 21:12:16 -0700

    refactored jquery/javascript. add isVowel function. add if branching to check for vowels and set pigLatin to appropriate concatenated string.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,32 @@
 //business logic
 var vowels = ["a", "e", "i", "o", "u"];
 
+var isVowel = function(letter) {
+  for(i = 0; i < vowels.length; i++){
+    if(letter === vowels[i]){
+        return true;
+    }
+  }
+  return false;
+};
+
 
 var pigLatin = function(inputtedString) {
 
   var firstLetter = inputtedString.charAt(0);
-  var pigString = inputtedString.slice(1) + firstLetter + "ay"; //consonant case
+  var secondLetter = inputtedString.charAt(1);//will return a null string if no second letter
+  var thirdLetter = inputtedString.charAt(2);//will return a null string if no third letter
+  var pigString = "";
 
-  vowels.forEach(function(vowel) {
-    if (vowel === firstLetter) {
-      pigString = inputtedString + "way"; //vowel case
-    }
-  });
-
+  if (isVowel(firstLetter)) { //all words with vowels will get "way"
+    pigString = inputtedString + "way";
+  } else if (!isVowel(firstLetter) && isVowel(secondLetter)) { //all words with one consonant will get consonant sliced and added to end and "ay" too.
+    pigString = inputtedString.slice(1) + firstLetter + "ay";
+  } else if (!isVowel(firstLetter) && !isVowel(secondLetter) && isVowel(thirdLetter)) {
+    pigString = inputtedString.slice(2) + firstLetter + secondLetter + "ay";
+  } else if (!isVowel(firstLetter) && !isVowel(secondLetter) && !isVowel(thirdLetter)) {
+    pigString = inputtedString.slice(3) + firstLetter + secondLetter + thirdLetter + "ay";
+  }
   return pigString;
 };
 
